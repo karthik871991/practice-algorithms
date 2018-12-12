729c8537964c45fd2ac45bb8c6f1a08a3735a6eb
karthik Selvaraj <karthik871991@gmail.com>
2018-12-12 00:02:44 -0500

    819

---
+++ ConsoleApp1/ConsoleApp1/MostCommonWord819.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoPractice
+{
+    public class MostCommonWord819
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            var hashSet = new HashSet<string>(banned);
+
+
+            var wordArr = paragraph
+                            .Replace("!", " ")
+                            .Replace("?", " ")
+                            .Replace("'", " ")
+                            .Replace(",", " ")
+                            .Replace(".", " ")
+                            .Replace(";", " ")
+                            .ToLower()
+                            .Split(' ');
+
+            var countWords = new Dictionary<string, int>();
+
+            foreach (var word in wordArr)
+            {
+                if (!string.IsNullOrWhiteSpace(word) && !hashSet.Contains(word))
+                {
+                    if (countWords.ContainsKey(word))
+                    {
+                        countWords[word]++;
+                    }
+                    else
+                    {
+                        countWords.Add(word, 1);
+                    }
+                }
+            }
+
+            int max = 0;
+            string result = string.Empty;
+
+            foreach (var word in countWords)
+            {
+                if (word.Value >= max)
+                {
+                    max = word.Value;
+                    result = word.Key;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Tests/MostCommonWord819Tests.cs
@@ -0,0 +1,33 @@
+﻿using AlgoPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class MostCommonWord819Tests
+    {
+        [TestMethod]
+        public void TestMostCommonWord1()
+        {
+            string[] banned = new string[] { "hit" };
+            string paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            var mcw = new MostCommonWord819();
+            var result = mcw.MostCommonWord(paragraph, banned);
+
+            Assert.AreEqual("ball", result);
+        }
+
+        [TestMethod]
+        public void TestMostCommonWord2()
+        {
+            string[] banned = new string[] { "a" };
+            string paragraph = "a, a, a, a, b,b,b,c, c";
+
+
+            var mcw = new MostCommonWord819();
+            var result = mcw.MostCommonWord(paragraph, banned);
+
+            Assert.AreEqual("b", result);
+        }
+    }
+}
