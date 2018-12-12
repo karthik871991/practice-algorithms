9afca52ddc8ef5c1527e3631b42390d7a9156914
karthik Selvaraj <karthik871991@gmail.com>
2018-12-11 19:00:46 -0500

    first change

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class AlgoPractice
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
