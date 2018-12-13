2d93ecf8e0b6ca2042cf49072a5c64f549788a03
karthik Selvaraj <karthik871991@gmail.com>
2018-12-13 18:53:31 -0500

    lru cache

---
+++ ConsoleApp1/ConsoleApp1/LRUCache146.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoPractice
+{
+    public class LRUCache
+    {
+        public Node Head = null;
+        public Node Tail = null;
+        public int Capacity = 0;
+        public Dictionary<int, Node> KeyValuePairs;
+
+        public LRUCache(int capacity)
+        {
+            Capacity = capacity;
+            KeyValuePairs = new Dictionary<int, Node>();
+
+            Head = new Node();
+            Tail = new Node();
+
+            Head.Previous = Tail;
+            Tail.Next = Head;
+        }
+
+        public int Get(int key)
+        {
+            if (KeyValuePairs.ContainsKey(key))
+            {
+                var node = KeyValuePairs[key];
+                Remove(key);
+                AddToFront(node.Key, node.Value);
+                return node.Value;
+            }
+            return 0;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (KeyValuePairs.ContainsKey(key))
+            {
+                Remove(key);
+                AddToFront(key, value);
+            }
+            else
+            {
+                AddToFront(key, value);
+                KeyValuePairs.Add(key, Head.Previous);                
+            }
+        }
+
+        private void Remove(int key)
+        {
+            var currentNode = KeyValuePairs[key];
+            KeyValuePairs.Remove(key);
+
+            currentNode.Previous.Next = currentNode.Next;
+            currentNode.Next.Previous = currentNode.Previous;
+        }
+
+        private void AddToFront(int key, int value)
+        {
+            var node = new Node { Key = key, Value = value };
+            var previous = Head.Previous;
+            node.Previous = previous;
+            node.Next = Head;
+            Head.Previous = node;
+            previous.Next = node;
+        }
+
+        public Node GetHead()
+        {
+            return Head.Previous;
+        }
+
+        public Node GetTail()
+        {
+            return Tail.Next;
+        }
+
+        public class Node
+        {
+            public Node Previous;
+            public int Key;
+            public int Value;
+            public Node Next;
+        }
+    }
+}
---
+++ ConsoleApp1/Tests/LRUCache146Tests.cs
@@ -0,0 +1,52 @@
+﻿using AlgoPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class LRUCache146Tests
+    {
+        [TestMethod]
+        public void LRUTests()
+        {
+            var cache = new LRUCache(20);
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            cache.Put(3, 3);
+            cache.Put(4, 4);
+            cache.Put(5, 5);
+            cache.Put(6, 6);
+
+            var head = cache.GetHead();
+            int count = 6;
+
+            while (head != null && head.Previous != null)
+            {
+                Assert.AreEqual(head.Value, count);
+                count--;
+                head = head.Previous;
+            }
+
+            var value = cache.Get(1);
+
+            Assert.AreEqual(1, value);
+
+            var list = new List<int> { 2, 3, 4, 5, 6, 1 };
+
+            var head1 = cache.GetHead();
+            int count1 = list.Count - 1;
+
+            while (head1 != null && count1 < list.Count && head1.Previous != null)
+            {
+                Assert.AreEqual(head1.Value, list[count1]);
+                count1--;
+                head1 = head1.Previous;
+            }
+
+
+        }
+    }
+}
