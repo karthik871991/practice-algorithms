c2c19c7a5b6b8625a1e8cf691d69bbf80e030f6b
karthik Selvaraj <karthik871991@gmail.com>
2019-01-06 23:56:09 -0500

    practice

--- ConsoleApp1/ConsoleApp1/PrisonCellsAfterNDays957.cs
+++ ConsoleApp1/ConsoleApp1/PrisonCellsAfterNDays957.cs
@@ -8,6 +8,11 @@ namespace AlgoPractice
     {
         public int[] PrisonAfterNDays(int[] cells, int N)
         {
+            var tree = new SortedDictionary<int, int>();
+            foreach (var t in tree)
+            {
+
+            }
             var map = new Dictionary<string, int[]>();
 
             var cellsCopy = new int[cells.Length];
---
+++ ConsoleApp1/Tests/Practice.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class Practice
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var a = new Sample { foo = 1, moo = 3 };
+            var b = new Sample { foo = 2, moo = 2 };
+            var c = new Sample { foo = 3, moo = 1 };
+            var d = new Sample { foo = 4, moo = 2 };
+            var e = new Sample { foo = 1, moo = 1 };
+            var f = new Sample { foo = 2, moo = 1 };
+            var g = new Sample { foo = 4, moo = 1 };
+            var h = new Sample { foo = 4, moo = 1 };
+
+            var list = new List<Sample> { a, b, c, d, e, f, g };
+
+            var sort = list.OrderBy(x => x.foo).ThenBy(y => y.moo);
+
+            var map = new Dictionary<int, int>();
+            
+            var sortSet = new SortedSet<Sample>(new DuplicateKeyComparer());
+
+            var sortedDict = new SortedDictionary<Sample, Sample>(new DuplicateKeyComparer());
+
+            foreach (var l in list)
+            {
+                sortSet.Add(l);
+                sortedDict.Add(l, l);
+            }
+        }
+
+        public class DuplicateKeyComparer : IComparer<Sample>
+        {
+
+            public int Compare(Sample x, Sample y)
+            {
+                int result = x.foo.CompareTo(y.foo);
+
+                if (result == 0)
+                {
+                    if (x.moo.CompareTo(y.moo) == 0)
+                    {
+                        return x.GetHashCode().CompareTo(y.GetHashCode());
+                    }
+                    return x.moo.CompareTo(y.moo);   // Handle equality as beeing greater
+                }
+                    
+                else
+                    return result;
+            }
+        }
+
+        public class Sample
+        {
+            public int foo { get; set; }
+            public int moo { get; set; }
+        }
+    }
+}
