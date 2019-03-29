ba5d5ced5d6436d33be413f7e0a1cdc98923455c
karthik Selvaraj <karthik871991@gmail.com>
2019-03-28 20:32:33 -0400

    asa

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -7,6 +9,133 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //retrieveMostFrequentlyUsedWords(null, null);
+
+            //GetStrings("awaglknagawunagwkwagl", 4);
+            //GetStrings("abcd", 3);
+
+            
+
+        }
+
+
+        public static int GetDayDate(int year)
+        {
+            var day = dayofweek(1, 10, year);
+
+            var num = day - 2;
+
+            int dayToCalculate = 1;
+
+            if (num < 0)
+            {
+                dayToCalculate += 7 + Math.Abs(day - 2);
+            }
+            else if (num > 0)
+            {
+                dayToCalculate += 7 + 7 - day + 2;
+            }
+            else
+            {
+                dayToCalculate += 7;
+            }
+
+
+            return dayToCalculate;
+        }
+
+        public static int dayofweek(int d, int m, int y)
+        {
+            int[] t = { 0, 3, 2, 5, 0, 3, 5,
+                            1, 4, 6, 2, 4 };
+            y -= (m < 3) ? 1 : 0;
+
+            return (y + y / 4 - y / 100 + y / 400
+                             + t[m - 1] + d) % 7;
+        }
+
+        public static List<string> GetStrings(string inputStr, int number)
+        {
+            var answer = new HashSet<string>();
+
+            for (int i = 0; i <= inputStr.Length - number; i++)
+            {
+                var subString = inputStr.Substring(i, number);
+
+                var l = subString.Distinct().Count();
+                if (subString.Length == subString.Distinct().Count())
+                {
+                    answer.Add(subString);
+                }
+
+            }
+
+            return answer.ToList();
+        }
+
+        public static List<String> retrieveMostFrequentlyUsedWords(String literatureText,
+                                                   List<String> wordsToExclude)
+        {
+            if (string.IsNullOrWhiteSpace(literatureText))
+            {
+                return new List<string>();
+            }
+            var hashset = new HashSet<string>();
+            if (!(wordsToExclude == null || wordsToExclude.Count == 0))
+            {
+                hashset = new HashSet<string>(wordsToExclude);
+            }
+
+
+            var wordArr = literatureText
+                            .Replace("!", " ")
+                            .Replace("?", " ")
+                            .Replace("'", " ")
+                            .Replace(",", " ")
+                            .Replace(".", " ")
+                            .Replace(";", " ")
+                            .ToLower()
+                            .Split(' ');
+
+            var countWords = new Dictionary<string, int>();
+
+            foreach (var actualWord in wordArr)
+            {
+                var word = actualWord.ToLower();
+                if (!string.IsNullOrWhiteSpace(word) && !hashset.Contains(word))
+                {
+                    if (countWords.ContainsKey(word))
+                    {
+                        countWords[word]++;
+                    }
+                    else
+                    {
+                        countWords.Add(word, 1);
+                    }
+                }
+            }
+
+            int max = 0;
+            string result = string.Empty;
+
+            var sorted = countWords.OrderByDescending(x => x.Value).GroupBy(y => y.Value);
+
+            var answer = new List<string>();
+
+            var words = sorted.FirstOrDefault();
+
+            if (words == null || !words.Any())
+            {
+                return new List<string>();
+            }
+
+            foreach (var item in sorted.First())
+            {
+                answer.Add(item.Key);
+            }
+
+            return answer;
         }
     }
 }
