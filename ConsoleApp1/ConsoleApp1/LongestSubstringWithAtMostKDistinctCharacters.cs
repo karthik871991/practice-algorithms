ae80ba3112d65c5c27e62446ed8e9a8cee5e8aa9
Karthik Selvaraj <karthikselvaraj@amrock.com>
2019-09-04 17:59:31 -0400

    k characters

---
+++ ConsoleApp1/ConsoleApp1/LongestSubstringWithAtMostKDistinctCharacters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoPractice
+{
+    public class LongestSubstringWithAtMostKDistinctCharacters
+    {
+        public int LengthOfLongestSubstringTwoDistinct(string s)
+        {
+            var i = 0;
+            var j = 0;
+            var result = 0;
+            var map = new Dictionary<char, int>();
+
+            for (; i < s.Length; i++)
+            {
+                if (!map.ContainsKey(s[i]))
+                {
+                    map.Add(s[i], 1);
+                }
+                else
+                {
+
+                }
+                result = Math.Max(result, i - j + 1);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ConsoleApp1/Tests/LongestSubstringWithAtMostKDistinctCharactersTests.cs
@@ -0,0 +1,33 @@
+﻿using AlgoPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class LongestSubstringWithAtMostKDistinctCharactersTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var input = "eceba";
+
+            var result = new LongestSubstringWithAtMostKDistinctCharacters().LengthOfLongestSubstringTwoDistinct(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var input = "ccaabbb";
+
+            var result = new LongestSubstringWithAtMostKDistinctCharacters().LengthOfLongestSubstringTwoDistinct(input);
+
+            Assert.AreEqual(5, result);
+        }
+
+    }
+}
