807595dacd3ba5630dfaab7ca96972e09fbce091
Karthik Selvaraj <karthikselvaraj@amrock.com>
2019-09-04 14:34:02 -0400

    ngest substring

---
+++ ConsoleApp1/ConsoleApp1/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoPractice
+{
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var max = 0;
+            var j = 0;
+            var map = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!map.ContainsKey(s[i]))
+                {
+                    map.Add(s[i], i);
+                }
+                else
+                {
+                    j = Math.Max(j, map[s[i]] + 1);
+                    map[s[i]] = i;
+                }
+
+                max = Math.Max(max, i - j + 1);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ ConsoleApp1/Tests/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,52 @@
+﻿using AlgoPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class LongestSubstringWithoutRepeatingCharactersTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var input = "pwwkew";
+
+            var result = new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var input = "abcabcbb";
+
+            var result = new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var input = "bbbbb";
+
+            var result = new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var input = "abba";
+
+            var result = new LongestSubstringWithoutRepeatingCharacters().LengthOfLongestSubstring(input);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
