74c3efcc62b67c5c2b1fcd2631ca8b3018b046c8
Karthik Selvaraj <karthikselvaraj@amrock.com>
2019-09-17 15:06:19 -0400

    custoom 2 sum

---
+++ ConsoleApp1/ConsoleApp1/CustomTwoSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoPractice
+{
+    public class CustomTwoSum
+    {
+        public List<int> TwoSum(List<int> nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            var temp = new int[2];
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if(dict.ContainsKey(nums[i]))
+                {
+                    if(nums[i] > max || nums[dict[nums[i]]] > max)
+                    {
+                        temp[0] = dict[nums[i]];
+                        temp[1] = i;
+                        max = Math.Max(nums[i], dict[nums[i]]);
+                    }
+                }
+                dict[target - nums[i]] = i;
+            }
+
+            return temp.ToList();
+        }
+    }
+}
