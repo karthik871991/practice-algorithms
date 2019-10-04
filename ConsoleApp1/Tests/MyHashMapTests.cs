ed6a406be13f9bf4d283519c2436e8e8770e9175
karthik Selvaraj <karthik871991@gmail.com>
2019-10-04 09:33:22 -0400

    adding generic hashmap

---
+++ ConsoleApp1/Tests/MyHashMapTests.cs
@@ -0,0 +1,26 @@
+﻿using AlgoPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class MyHashMapTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var map = new MyHashMap<int ,int>();
+            map.Put(1, 1);
+            map.Put(2, 2);
+            map.Put(3, 3);
+            map.Put(1, 1);
+            map.Put(1, 5);
+
+            Assert.AreEqual(5, map.Get(1));
+
+        }
+    }
+}
