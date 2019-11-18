c85cefbc0ed3fa71c1b3b6f8e2720c4eb7edbf3e
karthik Selvaraj <karthik871991@gmail.com>
2019-11-17 22:48:42 -0500

    adding more

---
+++ ConsoleApp1/ConsoleApp1/MS/LexicographicallySmallestString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoPractice.MS
+{
+    public class LexicographicallySmallestString
+    {
+        public string LexiSmallestString(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str) || str.Length == 1)
+                return str;
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] > str[i + 1])
+                {
+                    return str.Substring(0, i) + str.Substring(i + 1, str.Length - i - 1);
+                }
+            }
+
+            return str.Remove(str.Length - 1);
+        }
+    }
+}
---
+++ ConsoleApp1/Tests/MS/LexicographicallySmallestStringTests.cs
@@ -0,0 +1,60 @@
+﻿using AlgoPractice.MS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.MS
+{
+    [TestClass]
+    public class LexicographicallySmallestStringTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = new LexicographicallySmallestString().LexiSmallestString("abczd");
+
+            Assert.AreEqual("abcd", result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = new LexicographicallySmallestString().LexiSmallestString("abcde");
+
+            Assert.AreEqual("abcd", result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var result = new LexicographicallySmallestString().LexiSmallestString("");
+
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var result = new LexicographicallySmallestString().LexiSmallestString("ba");
+
+            Assert.AreEqual("a", result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var result = new LexicographicallySmallestString().LexiSmallestString("abcded");
+
+            Assert.AreEqual("abcdd", result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var result = new LexicographicallySmallestString().LexiSmallestString("dcba");
+
+            Assert.AreEqual("cba", result);
+        }
+    }
+}
