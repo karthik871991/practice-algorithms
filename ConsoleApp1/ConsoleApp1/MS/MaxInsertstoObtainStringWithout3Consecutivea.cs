84605c2800045b9ee9c814eb04aaa403b4e68104
Karthik Selvaraj <karthikselvaraj@amrock.com>
2019-11-19 15:44:41 -0500

    adding more

---
+++ ConsoleApp1/ConsoleApp1/MS/MaxInsertstoObtainStringWithout3Consecutivea.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgoPractice.MS
+{
+    public class MaxInsertstoObtainStringWithout3Consecutivea
+    {
+        public int MaxInserts(string str)
+        {
+            var countA = 0;
+
+            var result = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    countA++;
+                }
+                else
+                {
+                    if (countA > 2)
+                        return -1;
+
+                    result += 2 - countA;
+                    countA = 0;
+                }
+            }
+
+            if (countA > 2)
+                return -1;
+
+            result += 2 - countA;
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Tests/MS/MaxInsertstoObtainStringWithout3ConsecutiveaTests.cs
@@ -0,0 +1,76 @@
+﻿using AlgoPractice.MS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.MS
+{
+    [TestClass]
+    public class MaxInsertstoObtainStringWithout3ConsecutiveaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = new MaxInsertstoObtainStringWithout3Consecutivea().MaxInserts("aabab");
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = new MaxInsertstoObtainStringWithout3Consecutivea().MaxInserts("dog");
+
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var result = new MaxInsertstoObtainStringWithout3Consecutivea().MaxInserts("aa");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var result = new MaxInsertstoObtainStringWithout3Consecutivea().MaxInserts("aaa");
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var result = new MaxInsertstoObtainStringWithout3Consecutivea().MaxInserts("baaa");
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var result = new MaxInsertstoObtainStringWithout3Consecutivea().MaxInserts("aaba");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            var result = new MaxInsertstoObtainStringWithout3Consecutivea().MaxInserts("aaa");
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            var result = new MaxInsertstoObtainStringWithout3Consecutivea().MaxInserts("aaa");
+
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
