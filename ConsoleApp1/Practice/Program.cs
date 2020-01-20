0b27708ad0ad6edaa8c9e01199f6780a09f95244
karthik Selvaraj <karthik871991@gmail.com>
2020-01-20 16:34:04 -0500

    max length

--- ConsoleApp1/Practice/Program.cs
+++ ConsoleApp1/Practice/Program.cs
@@ -14,6 +14,35 @@ namespace Practice
         {
 
         }
-        
+        public int MaxLength(List<string> list)
+        {
+            var maxLength = 0;
+
+            Explore(list, maxLength, 0, "");
+
+            return maxLength;
+        }
+
+        private static void Explore(List<string> list, int maxLength, int index, string path)
+        {
+            if (!IsUnique(path))
+            {
+                return;
+            }
+
+            var current = path.Length;
+            maxLength = current > maxLength ? current : maxLength;
+
+            for (int i = index; i < list.Count; i++)
+            {
+                Explore(list, maxLength, i + 1, path + list[i]);
+            }
+        }
+
+        private static bool IsUnique(string path)
+        {
+            var hashSet = new HashSet<char>(path);
+            return path.Length == hashSet.Count();
+        }
     }
 }
